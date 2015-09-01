bc03693350f59bf3760cf1dca6dc5a10dfc31189
andrelohmann <lohmann.andre@gmail.com>
2015-09-01 08:51:41 +0200

    update to git flow, SemVer and Silverstripe 3.2.x

--- code/controllers/BenchmarkController.php
+++ code/controllers/BenchmarkController.php
@@ -28,27 +28,34 @@ class BenchmarkController extends Controller {
 	 */
 	public function init() {
 		parent::init();
-                
-                if(!defined('BENCHMARK_KEY') || !isset($_SERVER['HTTP_BENCHMARK_KEY']) || $_SERVER['HTTP_BENCHMARK_KEY'] != BENCHMARK_KEY || !defined('BENCHMARK_SECRET') || !isset($_SERVER['HTTP_BENCHMARK_SECRET']) || $_SERVER['HTTP_BENCHMARK_SECRET'] != BENCHMARK_SECRET) return $this->httpError(404, _t('Benchmark.WRONGCREDENTIALS', 'Benchmark.WRONGCREDENTIALS'));
+		
+		if(!defined('BENCHMARK_KEY') || !isset($_SERVER['HTTP_BENCHMARK_KEY']) || $_SERVER['HTTP_BENCHMARK_KEY'] != BENCHMARK_KEY || !defined('BENCHMARK_SECRET') || !isset($_SERVER['HTTP_BENCHMARK_SECRET']) || $_SERVER['HTTP_BENCHMARK_SECRET'] != BENCHMARK_SECRET) return $this->httpError(404, _t('Benchmark.WRONGCREDENTIALS', 'Benchmark.WRONGCREDENTIALS'));
  	}
 
-        /**
-         * Default action if no other was specified.
-         * @action
-         * @todo maybe we can refactor this method later
-         */
-        public function index($request) {
-            if(class_exists('BenchmarkTest') && in_array("BenchmarkTestInterface", class_implements('BenchmarkTest'))){
-                $Content = BenchmarkTest::run();
-                
-                return $this->customise(new ArrayData(array(
-                    "Title" => _t('Benchmark.RUNSUCCESSTITLE', 'Benchmark.RUNSUCCESSTITLE'),
-                    "Content" => $Content
-                )))->renderWith(
-                    array('Benchmark_index', 'Benchmark', $this->stat('template_main'), 'BlankPage')
-                );
-            }else{
-                return $this->httpError(404, _t('Benchmark.BENCHMARKTESTMISSING', 'Benchmark.BENCHMARKTESTMISSING'));
-            }
+	/**
+	 * Returns a link to this controller.  Overload with your own Link rules if they exist.
+	 */
+	public function Link() {
+		return self::$url_segment .'/';
+	}
+
+    /**
+     * Default action if no other was specified.
+     * @action
+     * @todo maybe we can refactor this method later
+     */
+    public function index($request) {
+        if(class_exists('BenchmarkTest') && in_array("BenchmarkTestInterface", class_implements('BenchmarkTest'))){
+            $Content = BenchmarkTest::run();
+            
+            return $this->customise(new ArrayData(array(
+                "Title" => _t('Benchmark.RUNSUCCESSTITLE', 'Benchmark.RUNSUCCESSTITLE'),
+                "Content" => $Content
+            )))->renderWith(
+                array('Benchmark_index', 'Benchmark', $this->stat('template_main'), 'BlankPage')
+            );
+        }else{
+            return $this->httpError(404, _t('Benchmark.BENCHMARKTESTMISSING', 'Benchmark.BENCHMARKTESTMISSING'));
         }
+    }
 }
